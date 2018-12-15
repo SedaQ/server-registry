dcd891b6bd9750b3836de1cd574cdf55e0885920
Pavel Seda <pavelseda@email.cz>
2018-12-15 23:47:26 +0100

    Service registry with eureka server.

---
+++ src/main/java/com/sedaq/trainings/serviceregistry/ServiceRegistryApplication.java
@@ -0,0 +1,22 @@
+package com.sedaq.trainings.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * This is the example implementation of Eureka Server for service-registry (Used in microservices architecture)
+ *
+ * @author Pavel Seda
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegistryApplication.class, args);
+    }
+
+}
+
